118176496e1301b9a490c15c921ce5b98c5b9f7a
312312 <rohitsaini.aitm@gmail.com>
2022-01-28 01:13:11 -0800

    First

---
+++ src/main/java/newSetUp/newUp/App.java
@@ -0,0 +1,13 @@
+package newSetUp.newUp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/newSetUp/newUp/LoginPage.java
@@ -0,0 +1,22 @@
+package newSetUp.newUp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+
+	
+	@Test
+	public void loginPage()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\autoadmin\\Desktop\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://test.skytap.com");
+		
+		
+	}
+	
+	
+}
